55321a33a298a9c04786d4f54af34a49fb501907
Felix Cordero Jr <corderofelix101@gmail.com>
2023-06-23 10:45:02 +0800

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <link rel="stylesheet" href="/public_html/public/assets/css/style.css">
     <title>Felix-Header</title>
 </head>
 <body>
